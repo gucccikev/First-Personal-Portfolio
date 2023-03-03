0df956b5be9c896f520956dacb1df8eee5749858
gucccikev <71460353+gucccikev@users.noreply.github.com>
2023-03-03 21:24:16 +0100

    Little Changes

--- src/main.js
+++ src/main.js
@@ -5,6 +5,23 @@ function scrollBackOnTop() {
 }
 /*-----------------------------SCROLL BACK ON TOP-------------------------------*/
 
+function disableClick(){
+  document.onclick=function(event){
+  if (event.button == 2) {
+  alert('Right Click Message');
+  return false;
+    }
+  }
+}
+
+var message = "function disabled";
+
+function rtclickcheck(keyp){ if (navigator.appName == "Netscape" && keyp.which == 3){ alert(message); return false; }
+
+if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { alert(message); return false; } }
+
+document.onmousedown = rtclickcheck;
+
 /*-----------------------------SMOOTH SCROLL-------------------------------*/
 
 
@@ -36,12 +53,12 @@ function slideInFromRight()  {
   }, 4000);
 }
 
-function textFloatAnimation() {
+function onLoadFunction() {
   slideInFromLeft();
   slideInFromRight();
   currentSlide(1);
+  disableClick();
 }
-
 /*-----------------------------TEXT FLOAT IN ANIMATION-------------------------------*/
 
 /*-----------------------------CLICK ON DESIGNER-------------------------------*/
