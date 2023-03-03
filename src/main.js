047f0e9483b1335843e39e4522c7b76b1d8ea67e
gucccikev <71460353+gucccikev@users.noreply.github.com>
2023-03-03 15:39:53 +0100

    First and Second Page
    - Scroll Back To Top done
    - hrefs change color when hovered
    - cursor changed
    - content appears better onclick (DESIGNER&DEVELOPER)

--- src/main.js
+++ src/main.js
@@ -1,63 +1,13 @@
 /*-----------------------------SCROLL BACK ON TOP-------------------------------*/
-// document.querySelector('a[href="#top"]').addEventListener('click', function(e) {
-//   e.preventDefault();
-//   var target = document.querySelector('#top');
-//   var startPosition = window.pageYOffset;
-//   var distance = target.getBoundingClientRect().top;
-//   var duration = 800;
-//   var startTime = null;
-
-//   function animate(currentTime) {
-//     if (startTime === null) startTime = currentTime;
-//     var timeElapsed = currentTime - startTime;
-//     var run = ease(timeElapsed, startPosition, distance, duration);
-//     window.scrollTo(0, run);
-//     if (timeElapsed < duration) requestAnimationFrame(animate);
-//   }
-
-//   function ease(t, b, c, d) {
-//     t /= d / 2;
-//     if (t < 1) return c / 2 * t * t + b;
-//     t--;
-//     return -c / 2 * (t * (t - 2) - 1) + b;
-//   }
-
-//   requestAnimationFrame(animate);
-// });
+function scrollBackOnTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 /*-----------------------------SCROLL BACK ON TOP-------------------------------*/
 
 /*-----------------------------SMOOTH SCROLL-------------------------------*/
-// const navLinks = document.querySelectorAll('nav ul li a');
-// const sections = document.querySelectorAll('section');
-// const navHeight = document.querySelector('nav').offsetHeight;
 
-// navLinks.forEach(link => {
-//   link.addEventListener('click', e => {
-//     e.preventDefault();
-//     const targetId = link.getAttribute('href');
-//     const targetPosition = document.querySelector(targetId).offsetTop - navHeight;
-//     window.scrollTo({
-//       top: targetPosition,
-//       behavior: 'smooth'
-//     });
-//   });
-// });
 
-// window.addEventListener('scroll', () => {
-//   const currentPosition = window.pageYOffset;
-//   sections.forEach(section => {
-//     const sectionTop = section.offsetTop - navHeight;
-//     const sectionHeight = section.clientHeight;
-//     if (currentPosition >= sectionTop && currentPosition < sectionTop + sectionHeight) {
-//       navLinks.forEach(link => {
-//         link.classList.remove('active');
-//         if (link.getAttribute('href') === `#${section.id}`) {
-//           link.classList.add('active');
-//         }
-//       });
-//     }
-//   });
-// });
 /*-----------------------------SMOOTH SCROLL-------------------------------*/
 
 /*-----------------------------TEXT FLOAT IN ANIMATION-------------------------------*/
@@ -118,7 +68,7 @@ function displayDesignerInfo() {
     imageBlenderAndPhotoshop.style.width = designerSPANWidth + "px";
 
     designerInfo.style.display = "block";
-    designerSPAN.style.transition = "margin-top 0.25s ease-in-out";
+    designerSPAN.style.transition = "margin-top 0.5s ease-in-out";
   } else {
     designerSPAN.style.marginTop = "0px";
     designerInfo.style.marginTop = "0px";
@@ -189,7 +139,6 @@ function exhale(image) {
 /*-----------------------------HOVER WORKOUT-------------------------------*/
 
 /*-----------------------------DESIGNER SLIDESHOW-------------------------------*/
-
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -218,7 +167,6 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   line[slideIndex - 1].className += " active";
 }
-
 /*-----------------------------DESIGNER SLIDESHOW-------------------------------*/
 
 
